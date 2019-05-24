bf88563e126ff7712bdd83f10a67649a19913ed3
september3 <1296648354@qq.com>
2019-05-24 13:04:29 +0800

    commit  TransactionIsolationLevel

--- src/main/java/org/apache/ibatis/session/TransactionIsolationLevel.java
+++ src/main/java/org/apache/ibatis/session/TransactionIsolationLevel.java
@@ -1,32 +1,44 @@
-/**
- *    Copyright 2009-2018 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package org.apache.ibatis.session;
 
 import java.sql.Connection;
 
 /**
- * @author Clinton Begin
+ * 事务隔离级别
+ * @author Administrator
  */
 public enum TransactionIsolationLevel {
+  /**
+   * 不支持事务隔离
+   * TRANSACTION_NONE= 0;
+   */
   NONE(Connection.TRANSACTION_NONE),
+  /**
+   * 读已提交
+   * int TRANSACTION_READ_COMMITTED   = 2
+   * 当前隔离级别避免了脏读，不可重复读和幻象读会发生
+   */
   READ_COMMITTED(Connection.TRANSACTION_READ_COMMITTED),
+  /**
+   * 读未提交
+   * TRANSACTION_READ_UNCOMMITTED = 1
+   * 当前隔离级别会造成脏读、不可重复读、幻象读取
+   */
   READ_UNCOMMITTED(Connection.TRANSACTION_READ_UNCOMMITTED),
+  /**
+   * 重复读
+   * int TRANSACTION_REPEATABLE_READ  = 4
+   * 当前隔离级别避免了脏读和不可重复读，幻象读会发生
+   */
   REPEATABLE_READ(Connection.TRANSACTION_REPEATABLE_READ),
+  /**
+   * 序列化
+   *当前事务隔离级别避免了脏读、不可重复度、幻象读
+   */
   SERIALIZABLE(Connection.TRANSACTION_SERIALIZABLE);
 
+  /**
+   * 代表事务隔离级别
+   */
   private final int level;
 
   TransactionIsolationLevel(int level) {
