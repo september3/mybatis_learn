c1d31904d9488af007052fe08d722e990ea9d0d2
september3 <1296648354@qq.com>
2019-05-24 12:25:53 +0800

    commit  Environment

--- src/main/java/org/apache/ibatis/mapping/Environment.java
+++ src/main/java/org/apache/ibatis/mapping/Environment.java
@@ -1,18 +1,4 @@
-/**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
+
 package org.apache.ibatis.mapping;
 
 import javax.sql.DataSource;
@@ -20,13 +6,35 @@ import javax.sql.DataSource;
 import org.apache.ibatis.transaction.TransactionFactory;
 
 /**
- * @author Clinton Begin
+ *
+ * 该类相当于Configuration.XML文件中<environment></environment>标签，里面包含id、transactionFactory和dataSource
+ *
+ * 构建者模式：构建者模式一般用于构建复杂对象时，将复杂对象分割成许多小对象进行分别构建，然后整合在一起形成一个大对象，
+ * 这样做能很好的规范对象构建的细节过程
+ *
+ * 内部使用Builder建造者模式，只有当Environment对象中的内部构建方法builder（）被显示调用之后才会在内存中进行创建
+ * 实现了懒加载
  */
 public final class Environment {
+  /**
+   * 环境ID
+   */
   private final String id;
+  /**
+   * 事务工厂
+   */
   private final TransactionFactory transactionFactory;
+  /**
+   * 数据源
+   */
   private final DataSource dataSource;
 
+  /**
+   * 环境的带参数判断的构造函数
+   * @param id id
+   * @param transactionFactory transaction
+   * @param dataSource dataSource
+   */
   public Environment(String id, TransactionFactory transactionFactory, DataSource dataSource) {
     if (id == null) {
       throw new IllegalArgumentException("Parameter 'id' must not be null");
@@ -42,20 +50,37 @@ public final class Environment {
     this.dataSource = dataSource;
   }
 
+  /**
+   * 内部静态Builder---建造者模式
+   */
   public static class Builder {
     private String id;
     private TransactionFactory transactionFactory;
     private DataSource dataSource;
 
+    /**
+     * 构建Id
+     * @param id id
+     */
     public Builder(String id) {
       this.id = id;
     }
 
+    /**
+     * 构建事务工厂
+     * @param transactionFactory transactionFactory
+     * @return transactionFactory
+     */
     public Builder transactionFactory(TransactionFactory transactionFactory) {
       this.transactionFactory = transactionFactory;
       return this;
     }
 
+    /**
+     * 构建数据源
+     * @param dataSource dataSource
+     * @return dataSource
+     */
     public Builder dataSource(DataSource dataSource) {
       this.dataSource = dataSource;
       return this;
