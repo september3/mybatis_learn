cf36428ec796a0ad38329843f5cb91f69e71f73c
september3 <1296648354@qq.com>
2019-05-24 16:10:19 +0800

    commit  UnpooledDataSource

--- src/main/java/org/apache/ibatis/datasource/unpooled/UnpooledDataSource.java
+++ src/main/java/org/apache/ibatis/datasource/unpooled/UnpooledDataSource.java
@@ -1,18 +1,3 @@
-/**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package org.apache.ibatis.datasource.unpooled;
 
 import java.io.PrintWriter;
@@ -32,23 +17,60 @@ import javax.sql.DataSource;
 import org.apache.ibatis.io.Resources;
 
 /**
- * @author Clinton Begin
- * @author Eduardo Macarron
+ * 非池型数据源
+ * 非池型是相对池型而言的，池型数据源统筹管理着一个数据库连接池，在这个池中拥有指定数量的数据库连接实例connection可供使用，
+ * 其内部采用一定的规则指定数据连接对象的使用、创建、销毁规则，来为多线程数据库访问提供及时有效的数据库连接。
+ *
+ * 非池型数据源，即保持有一个数据库连接实例的数据源
+ *
+ * @author Administrator
  */
 public class UnpooledDataSource implements DataSource {
-
+  /**
+   * 驱动类加载器：目的是 从磁盘中加载数据库驱动到内存
+   */
   private ClassLoader driverClassLoader;
+  /**
+   * 驱动器属性：用以保存手动设置的数据库驱动属性，如果不进行设置，则使用默认值。
+   * 当前属性设置的键一般使用“driver.”为前缀
+   */
   private Properties driverProperties;
+  /**
+   * 数据库驱动注册器，其内部保存着所有已注册的数据库驱动类实例，
+   * 这个字段是static修饰的，表示在数据源类加载的时候就会创建，这个时候创建的其实是个空集合。
+   * 再者使用ConcurrentHashMap集合，这是一个线程安全的键值对型集合，它几乎可以与HashTable通用（二者都是线程安全的集合类）。
+   */
   private static Map<String, Driver> registeredDrivers = new ConcurrentHashMap<>();
 
+  /**
+   * 数据库驱动类名
+   */
   private String driver;
+  /**
+   * 数据库服务器URL地址
+   */
   private String url;
+  /**
+   * 数据库服务器连接用户名
+   */
   private String username;
+  /**
+   * 数据库服务器连接密码
+   */
   private String password;
 
+  /**
+   * 是否自动提交
+   */
   private Boolean autoCommit;
+  /**
+   * 默认的事务隔离级别
+   */
   private Integer defaultTransactionIsolationLevel;
 
+  /**
+   * 验证的目的是确保使用本类加载器获取的驱动器类与在registeredDrivers中保存的对应驱动类实例的类是同一类型
+   */
   static {
     Enumeration<Driver> drivers = DriverManager.getDrivers();
     while (drivers.hasMoreElements()) {
@@ -57,6 +79,12 @@ public class UnpooledDataSource implements DataSource {
     }
   }
 
+  /**
+   * 无参构造器
+   * 这个构造器在数据源工厂的无参构造器中被调用，用于创建一个空的UnpooledDataSource实例，
+   * 然后使用工厂类中的setProperties()方法，为这个空实例中的各个字段进行赋值（
+   * 采用上面提到的第二种方式进行读取配置信息并保存到实例中），从而创建一个饱满的实例，
+   */
   public UnpooledDataSource() {
   }
 
@@ -88,31 +116,63 @@ public class UnpooledDataSource implements DataSource {
     this.driverProperties = driverProperties;
   }
 
+  /**
+   * 获取数据源连接的方法
+   * 无参方法，内部使用默认的用户名与面目进行数据库连接
+   * @return
+   * @throws SQLException
+   */
   @Override
   public Connection getConnection() throws SQLException {
+    //真正的执行数据库连接并获取这个连接的方法
     return doGetConnection(username, password);
   }
 
+  /**
+   * 指定用户名与密码的获取数据源连接的方法
+   * 使用指定的用户名与密码进行数据库连接，并将该连接返回
+   * @param username
+   * @param password
+   * @return
+   * @throws SQLException
+   */
   @Override
   public Connection getConnection(String username, String password) throws SQLException {
     return doGetConnection(username, password);
   }
 
+  /**
+   * 表示设置数据源连接数据库的最长等待时间，以秒为单位
+   * @param loginTimeout
+   */
   @Override
   public void setLoginTimeout(int loginTimeout) {
     DriverManager.setLoginTimeout(loginTimeout);
   }
 
+  /**
+   * 表示获取数据源连接到数据库的最长等待时间
+   * @return
+   */
   @Override
   public int getLoginTimeout() {
     return DriverManager.getLoginTimeout();
   }
 
+  /**
+   * 设置数据源的日志输出者（log writer）为给定的一个PrintWriter实例
+   * getLogWriter()：获取数据源的日志输出者。
+   * @param logWriter
+   */
   @Override
   public void setLogWriter(PrintWriter logWriter) {
     DriverManager.setLogWriter(logWriter);
   }
 
+  /**
+   * 获取这个DataSource所使用的所有Logger的父Logger。
+   * @return
+   */
   @Override
   public PrintWriter getLogWriter() {
     return DriverManager.getLogWriter();
@@ -182,7 +242,15 @@ public class UnpooledDataSource implements DataSource {
     this.defaultTransactionIsolationLevel = defaultTransactionIsolationLevel;
   }
 
+  /**
+   * 用以连接数据库并且获取数据库连接的方法
+   * @param username 用户名
+   * @param password 密码
+   * @return 数据库连接
+   * @throws SQLException
+   */
   private Connection doGetConnection(String username, String password) throws SQLException {
+    //Properties属性变量用于存储传递的参数
     Properties props = new Properties();
     if (driverProperties != null) {
       props.putAll(driverProperties);
@@ -197,13 +265,21 @@ public class UnpooledDataSource implements DataSource {
   }
 
   private Connection doGetConnection(Properties properties) throws SQLException {
+    //初始化驱动器
     initializeDriver();
     Connection connection = DriverManager.getConnection(url, properties);
+    //数据库连接的配置
     configureConnection(connection);
     return connection;
   }
 
+  /**
+   * 驱动器初始化---同步方法
+   * 加载驱动器类，并将其注册到DriverManager中，同时将其保存到本实例的registeredDrivers中
+   * @throws SQLException
+   */
   private synchronized void initializeDriver() throws SQLException {
+    //判断驱动器
     if (!registeredDrivers.containsKey(driver)) {
       Class<?> driverType;
       try {
@@ -223,6 +299,11 @@ public class UnpooledDataSource implements DataSource {
     }
   }
 
+  /**
+   * 数据库连接配置
+   * @param conn
+   * @throws SQLException
+   */
   private void configureConnection(Connection conn) throws SQLException {
     if (autoCommit != null && autoCommit != conn.getAutoCommit()) {
       conn.setAutoCommit(autoCommit);
@@ -232,6 +313,9 @@ public class UnpooledDataSource implements DataSource {
     }
   }
 
+  /**
+   * 真正的驱动器动态代理类
+   */
   private static class DriverProxy implements Driver {
     private Driver driver;
 
--- src/main/java/org/apache/ibatis/datasource/unpooled/UnpooledDataSourceFactory.java
+++ src/main/java/org/apache/ibatis/datasource/unpooled/UnpooledDataSourceFactory.java
@@ -1,18 +1,3 @@
-/**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package org.apache.ibatis.datasource.unpooled;
 
 import java.util.Properties;
@@ -25,28 +10,46 @@ import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
 
 /**
- * @author Clinton Begin
+ * 非池型数据源工厂
+ * 数据源工厂的目的就是将配置文件中的配置内容配置到通过自己的构造器创建的具体数据源实例中，
+ * 再使用getDataSource()方法返回给调用方。
+ * @author Administrator
  */
 public class UnpooledDataSourceFactory implements DataSourceFactory {
 
+  /**
+   * 驱动属性前缀
+   */
   private static final String DRIVER_PROPERTY_PREFIX = "driver.";
+  /**
+   * 属性前缀的长度
+   */
   private static final int DRIVER_PROPERTY_PREFIX_LENGTH = DRIVER_PROPERTY_PREFIX.length();
 
   protected DataSource dataSource;
 
+  /**
+   * 构建数据源并赋值
+   */
   public UnpooledDataSourceFactory() {
     this.dataSource = new UnpooledDataSource();
   }
 
+  /**
+   * 设置数据源驱动属性
+   * @param properties properties
+   */
   @Override
   public void setProperties(Properties properties) {
     Properties driverProperties = new Properties();
     MetaObject metaDataSource = SystemMetaObject.forObject(dataSource);
     for (Object key : properties.keySet()) {
       String propertyName = (String) key;
+      //第一种情况：属性名称以driver.开始
       if (propertyName.startsWith(DRIVER_PROPERTY_PREFIX)) {
         String value = properties.getProperty(propertyName);
         driverProperties.setProperty(propertyName.substring(DRIVER_PROPERTY_PREFIX_LENGTH), value);
+        //第二种情况：元对象中拥有针对属性名的setter方法
       } else if (metaDataSource.hasSetter(propertyName)) {
         String value = (String) properties.get(propertyName);
         Object convertedValue = convertValue(metaDataSource, propertyName, value);
@@ -65,6 +68,13 @@ public class UnpooledDataSourceFactory implements DataSourceFactory {
     return dataSource;
   }
 
+  /**
+   * 值类型强转方法
+   * @param metaDataSource
+   * @param propertyName
+   * @param value
+   * @return
+   */
   private Object convertValue(MetaObject metaDataSource, String propertyName, String value) {
     Object convertedValue = value;
     Class<?> targetType = metaDataSource.getSetterType(propertyName);
