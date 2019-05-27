46eb4dc7eb66b4d043e2d7dfc46c3151f85182e0
september3 <1296648354@qq.com>
2019-05-27 13:06:50 +0800

    commit  JNDIDataSource

--- src/main/java/org/apache/ibatis/datasource/jndi/JndiDataSourceFactory.java
+++ src/main/java/org/apache/ibatis/datasource/jndi/JndiDataSourceFactory.java
@@ -1,18 +1,3 @@
-/**
- *    Copyright 2009-2016 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package org.apache.ibatis.datasource.jndi;
 
 import java.util.Map.Entry;
@@ -27,12 +12,23 @@ import org.apache.ibatis.datasource.DataSourceException;
 import org.apache.ibatis.datasource.DataSourceFactory;
 
 /**
- * @author Clinton Begin
+ *托管型数据源
+ *
+ * @author Administrator
  */
 public class JndiDataSourceFactory implements DataSourceFactory {
 
+  /**
+   * 初始化上下文
+   */
   public static final String INITIAL_CONTEXT = "initial_context";
+  /**
+   * 数据源
+   */
   public static final String DATA_SOURCE = "data_source";
+  /**
+   * 环境变量前缀
+   */
   public static final String ENV_PREFIX = "env.";
 
   private DataSource dataSource;
@@ -61,6 +57,10 @@ public class JndiDataSourceFactory implements DataSourceFactory {
     }
   }
 
+  /**
+   * 获取外部创建的数据源实例
+   * @return
+   */
   @Override
   public DataSource getDataSource() {
     return dataSource;
