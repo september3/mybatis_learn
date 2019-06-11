c5afdfea6f06450d8b25e72fe98e9448be114dd8
september3 <1296648354@qq.com>
2019-06-11 17:16:15 +0800

    commit logging

--- src/main/java/org/apache/ibatis/logging/LogFactory.java
+++ src/main/java/org/apache/ibatis/logging/LogFactory.java
@@ -1,30 +1,17 @@
-/**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package org.apache.ibatis.logging;
 
 import java.lang.reflect.Constructor;
 
 /**
+ * 日志工厂
+ * 采用工厂模式
  * @author Clinton Begin
  * @author Eduardo Macarron
  */
 public final class LogFactory {
 
   /**
-   * Marker to be used by logging implementations that support markers.
+   * 标记将由支持标记的日志记录实现使用
    */
   public static final String MARKER = "MYBATIS";
 
@@ -39,6 +26,9 @@ public final class LogFactory {
     tryImplementation(LogFactory::useNoLogging);
   }
 
+  /**
+   * 防止被实例化
+   */
   private LogFactory() {
     // disable construction
   }
